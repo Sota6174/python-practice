777623103c63a6453f02459931262c958bb7f7af
Sota6174 <t119141som110223@gmail.com>
2021-03-05 00:52:53 +0900

    Add the ability to reply

--- DiscodeBot/test2.py
+++ DiscodeBot/test2.py
@@ -15,14 +15,28 @@ async def on_ready():
 
 
 # メッセージ受信時に動作する処理
+# @client.event
+# async def on_message(message):
+#     # メッセージ送信者がBotだった場合は無視する
+#     if message.author.bot:
+#         return
+#     # 「/neko」と発言したら「にゃーん」が返る処理
+#     if message.content == '/neko':
+#         await message.channel.send('にゃーん')
+
+
+# 話しかけた人にBotが返事をする機能
+async def reply(message):
+    # 返信する非同期関数
+    reply = f'{message.author.mention} 呼んだ?'
+    await message.channel.send(reply)   # 返信メッセージを送信
+
+
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    # メッセージのメンションにBotの名前があった時
+    if client.user in message.mentions:
+        await reply(message)
 
 
 # Botの起動とDiscordサーバーへの接続（このファイルが実行されたとき）
